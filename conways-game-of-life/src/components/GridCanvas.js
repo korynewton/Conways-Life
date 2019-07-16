a20986a8eb645dcf65b32b3d16c71cf435fe6939
Kory Newton <korybnewton@gmail.com>
2019-07-16 14:18:51 -0700

    clicking cell  WIP, edits hardcoded pixel

--- conways-game-of-life/src/components/GridCanvas.js
+++ conways-game-of-life/src/components/GridCanvas.js
@@ -7,6 +7,79 @@ export default class GridCanvas extends Component {
     this.state = {};
   }
 
+  getPixel(imageData, x, y) {
+    const w = imageData.width; // Conveniently the width is here
+    const h = imageData.height;
+
+    if (x < 0 || x >= w || y < 0 || y >= h) {
+      // Out of bounds
+      return null;
+    }
+
+    // Compute index within the array
+    const index = (w * y + x) * 4;
+
+    // Return a copy of the R, G, B, and A elements
+    return imageData.data.slice(index, index + 4);
+  }
+
+  setPixel(imageData, edited, x, y) {
+    // find canvas element, save as variable
+    const canvas = this.refs.canvas;
+    // creating a drawing object for our canvas
+    const ctx = canvas.getContext('2d');
+
+    const w = imageData.width; // Conveniently the width is here
+    const h = imageData.height;
+
+    if (x < 0 || x >= w || y < 0 || y >= h) {
+      // Out of bounds
+      return null;
+    }
+
+    // Compute index within the array
+    let index = (w * y + x) * 4;
+
+    for (let i = 0; i < 4; i++) {
+      imageData.data[index] = edited[i];
+      index++;
+    }
+
+    ctx.putImageData(imageData, 0, 0);
+
+    // Return a copy of the R, G, B, and A elements
+    // return imageData.data.slice(index, index + 4);
+  }
+
+  handleClick(e) {
+    // find canvas element, save as variable
+    const canvas = this.refs.canvas;
+    // creating a drawing object for our canvas
+    const ctx = canvas.getContext('2d');
+    const imageData = ctx.getImageData(
+      0,
+      0,
+      this.props.width,
+      this.props.height
+    );
+
+    const pixelRGBA = this.getPixel(imageData, 10, 10);
+    pixelRGBA[0] = 0;
+    pixelRGBA[1] = 0x00;
+    pixelRGBA[2] = 0x00;
+    pixelRGBA[3] = 0xff;
+
+    this.setPixel(imageData, pixelRGBA, 10, 10);
+    console.log('pixel data: ', pixelRGBA);
+
+    console.log('x:', e.screenX);
+    console.log('y:', e.screenY);
+
+    // ctx.moveTo(e.screenX, e.screenY);
+    // ctx.fill();
+    console.log('clickeroni');
+  }
+
   componentDidMount() {
     // destructuring props
     const { width, height, cellHeight, cellWidth } = this.props;
@@ -59,6 +132,7 @@ export default class GridCanvas extends Component {
           ref="canvas"
           width={this.props.width + 2}
           height={this.props.height + 2}
+          onClick={e => this.handleClick(e)}
         />
       </div>
     );
